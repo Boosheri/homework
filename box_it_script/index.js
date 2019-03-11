f48239bfaf37f742b61769bde46c5468865773ca
Nina Boosheri <nboosheri@gmail.com>
2019-03-11 15:09:33 -0700

    create drawLine function

---
+++ box_it_script/index.js
@@ -0,0 +1,12 @@
+//top/bottom line
+
+function drawLine(num){
+    let line = ""
+      for (let i=0; i<num; i++){
+          line += '\u2501'
+      }
+    return line
+  }
+  
+  //console.log(drawLine(8))
+  
