a6357bbb9d64753213616e556545291ac7e27ecf
Nina Boosheri <nboosheri@gmail.com>
2019-03-11 16:50:07 -0700

    create border functions

--- box_it_script/index.js
+++ box_it_script/index.js
@@ -1,12 +1,37 @@
-//top/bottom line
+// let length = process.argv[2].length
 
 function drawLine(num){
-    let line = ""
-      for (let i=0; i<num; i++){
-          line += '\u2501'
-      }
-    return line
-  }
-  
-  //console.log(drawLine(8))
-  
+  let line = ""
+    for (let i=0; i<num; i++){
+        line += "\u2501"
+    }
+  return line
+}
+//console.log(drawLine(8))
+
+let num = 1 //have to go back and set the funtion based on length eventually
+
+function drawTopBorder (){
+  let topLeftCorner = "\u250F"
+  let topRightCorner = "\u2513"
+  return topLeftCorner + drawLine(num) + topRightCorner
+}
+//console.log (drawTopBorder())
+
+function drawMiddleBorder (){
+  let middleLeft = "\u2523"
+  let middleRight = "\u252B"
+  return middleLeft + drawLine(num) + middleRight
+}
+//console.log (drawMiddleBorder())
+
+function drawBottomBorder (){
+  let bottomLeftCorner = "\u2517"
+  let bottomRightCorner = "\u251B"
+  return bottomLeftCorner + drawLine(num) + bottomRightCorner
+}
+//console.log (drawBottomBorder())
+
+function drawBarsAround (){}
+
+function drawBarsAround (){}
